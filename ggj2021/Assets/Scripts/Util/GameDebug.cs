3f34e54368dc6f946f0b9346ae312097c24ed710
Hugo Borges <hugouchoas@outook.com>
2021-01-26 19:31:34 -0300

    Added GameDebugger (better debug log)

---
+++ ggj2021/Assets/Scripts/Util/GameDebug.cs
@@ -0,0 +1,92 @@
+﻿/*
+ * Created by Hugo Uchoas Borges <hugouchoas@outlook.com>
+ */
+
+//#define DEVELOPER
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace util
+{
+    public class GameDebug : Singleton<GameDebug>
+    {
+        // ----------------------------------------------------------------------------------
+        // ========================== Logging Methods ============================
+        // ----------------------------------------------------------------------------------
+
+        [SerializeField] private bool systemLogEnabled = true;
+
+        private static void InternalLog(string message, Action<string> logCallback, LogType logType = LogType.None)
+        {
+            if (!LogTypeEnabled(logType)) return;
+
+            if (logType != LogType.None)
+                message = $"[{logType.ToString().ToUpper()}] " + message;
+
+            logCallback.Invoke(message);
+        }
+
+        public static void Log(string message, LogType logType = LogType.None)
+        {
+            InternalLog(message, Debug.Log, logType);
+        }
+
+        public static void LogWarning(string message, LogType logType = LogType.None)
+        {
+            InternalLog(message, Debug.LogWarning, logType);
+        }
+
+        public static void LogError(string message, LogType logType = LogType.None)
+        {
+            InternalLog(message, Debug.LogError, logType);
+        }
+
+
+        // ----------------------------------------------------------------------------------
+        // ========================== Logging Checkers ============================
+        // ----------------------------------------------------------------------------------
+
+        [Space]
+        [SerializeField] private List<LogType> logTypesEnabled;
+
+        private void Awake()
+        {
+            updateLogConf();
+        }
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            updateLogConf();
+        }
+#endif
+
+        private void updateLogConf()
+        {
+#if UNITY_EDITOR || DEVELOPER
+            Debug.unityLogger.logEnabled = systemLogEnabled;
+#elif UNITY_WEBGL && DEVELOPER
+            Debug.unityLogger.logEnabled = systemLogEnabled;
+#else
+            Debug.unityLogger.logEnabled = false;
+#endif
+        }
+
+        private static bool LogTypeEnabled(LogType logType)
+        {
+            return Instance.logTypesEnabled.Contains(logType);
+        }
+
+    }
+
+    public enum LogType
+    {
+        None,
+        Click,
+        Audio,
+        Scene,
+        Player,
+    }
+}
---
+++ ggj2021/Assets/Scripts/Util/Util.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by Hugo Uchoas Borges <hugouchoas@outlook.com>
+ */
+
+namespace util
+{
+    public class Util
+    {
+        public static bool IsEditor()
+        {
+#if UNITY_EDITOR
+            return true;
+#endif
+            return false;
+        }
+
+        public static bool IsWebGL()
+        {
+#if UNITY_WEBGL
+            return true;
+#endif
+            return false;
+        }
+
+        public static bool IsWindows()
+        {
+#if UNITY_WINDOWS
+            return true;
+#endif
+            return false;
+        }
+    }
+}
