ad338a2fcff9f3f6dd9f42daafd96962b0222c64
Dan Domingues <dom.dev.contact@gmail.com>
2021-01-30 13:58:55 -0300

    Added 2D paralax component, changed camera to FixedUpdate for smoother movement

--- ggj2021/Assets/Scripts/Camera/CameraController.cs
+++ ggj2021/Assets/Scripts/Camera/CameraController.cs
@@ -2,28 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace camera {
-
-    public class CameraController : MonoBehaviour {
+namespace camera 
+{
+    public class CameraController : MonoBehaviour
+    {
         [SerializeField] private Transform follow;
         [SerializeField] private float velocity;
+        [SerializeField] ParalaxBackground[] backgrounds;
+
         private Vector3 initialOffset;
         private float lastScaleX = 1;
 
-        void Start() {
+        void Start()
+        {
             initialOffset = transform.position - follow.position;
             lastScaleX = Mathf.Sign(transform.localScale.x);
         }
 
-        void Update() {
-            if (Mathf.Sign(follow.localScale.x) != lastScaleX) {
+        void FixedUpdate()
+        {
+            if (Mathf.Sign(follow.localScale.x) != lastScaleX)
+            {
                 initialOffset.x *= -1f;
                 lastScaleX *= -1f;
             }
 
             var direction = (follow.position + initialOffset) - transform.position;
+            var delta = direction * velocity * Time.fixedDeltaTime;
 
-            transform.position += direction * velocity * Time.deltaTime;
+            for (int i = 0; i < backgrounds.Length; i++)
+            {
+                backgrounds[i].UpdateMovement(delta);
+            }
+
+            transform.position += delta;
         }
     }
 }
---
+++ ggj2021/Assets/Scripts/Objects/ParalaxBackground.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParalaxBackground : MonoBehaviour
+{
+    [SerializeField] float movementMultiplier;
+
+    public void UpdateMovement(Vector3 cameraDelta)
+    {
+        transform.position -= new Vector3(cameraDelta.x * movementMultiplier, 0, 0);
+    }
+}
