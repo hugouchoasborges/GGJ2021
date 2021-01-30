5e43deb84ba128cc5863a6ba281f3468684dccc5
jvmantovani <joao.mantovani@hydrasystems.com.br>
2021-01-30 00:27:01 -0300

    Basic following camera

---
+++ ggj2021/Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform follow;
+    [SerializeField] private float velocity;
+    private Vector3 initialOffset;
+    private float lastScaleX = 1;
+
+    void Start()
+    {
+        initialOffset = transform.position - follow.position;
+        lastScaleX = Mathf.Sign(transform.localScale.x);
+    }
+
+    void Update()
+    {
+        if (Mathf.Sign(follow.localScale.x) != lastScaleX) {
+            initialOffset.x *= -1f;
+            lastScaleX *= -1f;
+        }
+
+        var direction = (follow.position + initialOffset) - transform.position;
+
+        transform.position += direction * velocity * Time.deltaTime;
+    }
+}
