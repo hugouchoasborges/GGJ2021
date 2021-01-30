a109e29d282a2fbaeb05afccd2e8be94f20539da
Hugo Borges <hugouchoas@outook.com>
2021-01-29 21:00:41 -0300

    Base player jump

---
+++ ggj2021/Assets/Scripts/Player/JumpBehavior.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using util;
+
+namespace player
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class JumpBehavior : MonoBehaviour
+    {
+        [Header("Jump controls")]
+        [SerializeField]
+        [Range(1f, 10)] private float _jumpImpulse = 5f;
+
+        [Header("Input Keys (string)")]
+        [SerializeField] private string _jumpInputKey = "Jump";
+
+        private Rigidbody2D _rigidbody2D;
+
+        private void Awake()
+        {
+            _rigidbody2D = GetComponent<Rigidbody2D>();
+        }
+
+        private void WalkInput()
+        {
+            if (Input.GetButtonDown(_jumpInputKey))
+            {
+                GameDebug.Log("JUMP", util.LogType.Player);
+
+                _rigidbody2D.AddForce(Vector2.up * _jumpImpulse, ForceMode2D.Impulse);
+            }
+        }
+
+        private void Update()
+        {
+            WalkInput();
+        }
+    }
+}
--- ggj2021/Assets/Scripts/Player/PlayerController.cs
+++ ggj2021/Assets/Scripts/Player/PlayerController.cs
@@ -8,11 +8,13 @@ namespace player
     public class PlayerController : MonoBehaviour
     {
         private WalkBehavior _walkBehavior;
+        private JumpBehavior _jumpBehavior;
         private RespawnBehavior _respawnBehavior;
 
         private void Awake()
         {
             _walkBehavior = GetComponent<WalkBehavior>();
+            _jumpBehavior = GetComponent<JumpBehavior>();
             _respawnBehavior = GetComponent<RespawnBehavior>();
         }
     }
