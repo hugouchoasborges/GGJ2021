143ee1d8736e9268f480edadca8ab59319d96b86
jvmantovani <joao.mantovani@hydrasystems.com.br>
2021-01-31 18:30:17 -0300

    Game scene

---
+++ ggj2021/Assets/Cloud.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud : MonoBehaviour
+{
+    public float speed = 0;
+
+    private void FixedUpdate() {
+        transform.position += Vector3.right * speed * Time.fixedDeltaTime;
+    }
+
+}
